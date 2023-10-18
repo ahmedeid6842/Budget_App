aeded85e8a4a758fddf2f71b5f8dd4cb0b2d7abd
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-18 11:59:59 +0300

    Added group => users realtion

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_18_075152) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_18_085532) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "deals", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "amount", default: "0", null: false
+    t.integer "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "icon"
+    t.integer "author_id", null: false
+    t.integer "deal_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +44,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_18_075152) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "deals", "users", column: "author_id", on_delete: :cascade
+  add_foreign_key "groups", "deals", on_delete: :cascade
 end
