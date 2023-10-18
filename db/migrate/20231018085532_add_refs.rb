73d98a758a0faaadebe90af19f552f71be1fcc21
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-18 11:56:34 +0300

    Adding references between tables

---
+++ db/migrate/20231018085532_add_refs.rb
@@ -0,0 +1,6 @@
+class AddRefs < ActiveRecord::Migration[7.0]
+  def change
+    add_foreign_key :deals, :users, column: :author_id, on_delete: :cascade
+    add_foreign_key :groups, :deals, column: :deal_id, on_delete: :cascade
+  end
+end
