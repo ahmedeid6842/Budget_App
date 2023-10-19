c0d96cbfc91b9f9c6cce4d39f5cd15afc524ad9f
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-19 05:53:43 +0300

    Added index to group controller

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -1,3 +1,7 @@
 class GroupController < ApplicationController
   before_action :set_group, only: %i[show edit update destroy]
+
+  def index
+    @groups = Group.where(author_id: current_user.id)
+  end
 end
