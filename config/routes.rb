4b2ad90458ad22a2b089421a7f90b75600b338a6
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-19 05:38:00 +0300

    Setup the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: "home#index" 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "home#index"
+
+  authenticated :user do
+    root to: "groups#index", as: :authenticated_root
+  end
+
+  resources :groups, only: [:new, :create, :index] do
+    resources :deals, only: [:new, :create, :index]
+  end
 end
