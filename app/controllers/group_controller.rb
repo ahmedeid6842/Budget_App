ead324e0a7f92188f89aef17c3f45e6aa6010e71
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-19 05:54:26 +0300

    Added create to group controlelr

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -4,4 +4,19 @@ class GroupController < ApplicationController
   def index
     @groups = Group.where(author_id: current_user.id)
   end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = current_user.groups.new(group_params)
+
+    if @group.save
+      redirect_to groups_path, notice: "Category has been created!"
+    else
+      flash[:alert] = "Something went wrong, Try again!"
+      render :new
+    end
+  end
 end
