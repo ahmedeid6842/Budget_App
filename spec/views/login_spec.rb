663bd980c27270e6d54bfb5014bb4b238754da81
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-10-19 08:29:59 +0300

    Fixed Log in button name

--- spec/views/login_spec.rb
+++ spec/views/login_spec.rb
@@ -16,13 +16,13 @@ RSpec.describe "Log In page", type: :feature do
   end
 
   it "should see the login button" do
-    expect(page).to have_button "Log In"
+    expect(page).to have_button "Log in"
   end
 
   it "should be able to see authenticated route" do
     fill_in "Email", with: @user1.email
     fill_in "Password", with: @user1.password
-    click_button "Log In"
+    click_button "Log in"
     expect(page).to have_content("CATEGORIES")
   end
 end
